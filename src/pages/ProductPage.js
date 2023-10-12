d8b44d21a971ec1dc31d9a37c3ad2d6066ae94a8
Maxwell Calkin <maxwellcalkin@gmail.com>
2023-10-12 12:01:16 -0400

    change product page styling

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -11,51 +11,8 @@ import {
   CardContent,
   CardMedia,
   Paper,
+  Box, // Import Box component for spacing
 } from "@mui/material";
-
-const styles = {
-  productImage: {
-    width: "100%",
-    maxHeight: "400px",
-    objectFit: "contain",
-  },
-  productInfo: {
-    padding: "20px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-start",
-    justifyContent: "space-between",
-    height: "100%",
-    backgroundColor: "whitesmoke",
-    borderRadius: "8px",
-    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
-    marginBottom: "20px",
-    paddingTop: "20px",
-    paddingBottom: "20px",
-    paddingRight: "20px",
-    paddingLeft: "20px",
-    transition: "transform 0.2s",
-    "&:hover": {
-      transform: "scale(1.02)",
-    },
-  },
-  description: {
-    marginBottom: "20px",
-  },
-  price: {
-    fontWeight: "bold",
-    fontSize: "24px",
-    marginBottom: "20px",
-  },
-  buttons: {
-    display: "flex",
-    justifyContent: "space-between",
-    marginTop: "20px",
-  },
-  cartButton: {
-    marginLeft: "15px", // Add margin to create space
-  },
-};
 
 class ProductPage extends Component {
   render() {
@@ -79,52 +36,80 @@ class ProductPage extends Component {
                 alt={product.name}
                 height="400"
                 image={product.image}
-                style={styles.productImage}
+                sx={{ width: "100%", maxHeight: "400px", objectFit: "contain" }}
               />
             </Card>
           </Grid>
           <Grid item xs={12} md={6}>
-            <Paper elevation={3} style={styles.productInfo}>
+            <Paper
+              elevation={3}
+              sx={{
+                padding: "20px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                justifyContent: "space-between",
+                height: "100%",
+                backgroundColor: "whitesmoke",
+                borderRadius: "8px",
+                boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+                marginBottom: "20px",
+                paddingTop: "20px",
+                paddingBottom: "20px",
+                paddingRight: "20px",
+                paddingLeft: "20px",
+                transition: "transform 0.2s",
+                "&:hover": {
+                  transform: "scale(1.02)",
+                },
+              }}
+            >
               <Typography variant="h4" component="h1">
                 {product.name}
               </Typography>
               <Typography
                 variant="h6"
                 color="textSecondary"
-                style={styles.description}
+                sx={{ marginBottom: "20px" }}
               >
                 {product.description}
               </Typography>
-              <Typography variant="h6" style={styles.price}>
+              <Typography
+                variant="h6"
+                sx={{
+                  fontWeight: "bold",
+                  fontSize: "24px",
+                  marginBottom: "20px",
+                }}
+              >
                 Price: ${product.price}
               </Typography>
-              <div style={styles.buttons}>
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
                 <Button
                   variant="contained"
                   color="primary"
                   onClick={() => addToCart(product)}
+                  sx={{ marginRight: "10px" }}
                 >
                   Add to Cart
                 </Button>
-                <Link to="/cart">
-                  <Button
-                    variant="contained"
-                    color="secondary"
-                    style={styles.cartButton}
-                  >
-                    Go to Cart
-                  </Button>
-                </Link>
-                <Link to="/">
-                  <Button
-                    variant="contained"
-                    color="secondary"
-                    style={styles.cartButton}
-                  >
-                    Back to Product List
-                  </Button>
-                </Link>
-              </div>
+                <Box>
+                  <Link to="/cart">
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      sx={{ marginRight: "10px" }}
+                    >
+                      Go to Cart
+                    </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button variant="contained" color="secondary">
+                      Back to Product List
+                    </Button>
+                  </Link>
+                </Box>
+              </Box>
             </Paper>
           </Grid>
         </Grid>
